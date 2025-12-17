786cac343ad564f352a6247cfa546590a9f959cd
HP <fasilrahman96@gmail.com>
2025-12-17 12:24:21 +0530

    completed room controller && room model

---
+++ backend/src/router/room_router.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { adminRoute, protectRoute } from '../middleware/auth_middleware';
+import { createRoom, deleteRoom, getAllRooms, getSingleRoom, updateRoom } from '../controllers/room_controller';
+
+
+const router = express.Router();
+
+app.use(protectRoute);
+
+
+router.post("/create", adminRoute, createRoom);
+router.get("/fetchAll", getAllRooms);
+router.get("/fetch/:id", getSingleRoom);
+router.put("edit/:id" , adminRoute , updateRoom);
+router.delete("/delete/:id" , adminRoute , deleteRoom);
+
+
+
+
+
+export default router;
